64fbb70aa6d44646593b1da5d7d946de66dc51d0
thesubconscious <460356823@qq.com>
2024-07-04 21:03:24 +0800

    Work in progress

--- e_r_platform/src/main/java/com/e_r_platform/service/impl/UserServiceImpl.java
+++ e_r_platform/src/main/java/com/e_r_platform/service/impl/UserServiceImpl.java
@@ -2,11 +2,19 @@ package com.e_r_platform.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.e_r_platform.controller.JwtHandler;
 import com.e_r_platform.model.User;
 import com.e_r_platform.service.UserService;
 import com.e_r_platform.mapper.UserMapper;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 /**
 * @author Administrator
@@ -16,12 +24,30 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User>
     implements UserService{
-
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
     @Autowired
     private UserMapper userMapper;
+    @Autowired
+    private JwtHandler jwtHandler;
+    @Autowired
+    private CustomUserDetailService userDetailService;
+
+    User checkUserStorage(User user){
+        String cacheKey = "user:" + user.getUser_id();
+        User existedUser = (User)redisTemplate.opsForValue().get(cacheKey);
+        if (existedUser == null) {
+            Optional<User> optionalUser = userMapper.selectUserByEmail(user.getEmail());
+            if (optionalUser.isPresent()) {
+                existedUser = optionalUser.get();
+                redisTemplate.opsForValue().set(cacheKey, existedUser); // 重新缓存数据
+            }
+        }
+        return existedUser;
+    }
 
     public int register(User user){
-        User existedUser =  userMapper.selectUserByEmail(user.getEmail());
+        User existedUser =  checkUserStorage(user);
         if(existedUser!=null){
             return -1;
         }
@@ -34,18 +60,19 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User>
     }
 
     public String login(String email, String password) {
-        User user = userMapper.login(email,password);
-        if(user == null) return null;
-
-        int token = user.hashCode();
-        user.setToken(String.valueOf(token));
-        userMapper.updateUserToken(user.getEmail(),user.getToken());
-        return user.getToken();
+        User user = userMapper.login(email, password);
+        if (user != null) {
+            return jwtHandler.generateToken(userDetailService.loadUserByUsername(email));
+        }
+        return null;
     }
 
-    public int logOut(String token){
-        int result = userMapper.deleteUserToken(token);
-        return result;
+    public void logOut(HttpServletResponse response){
+        Cookie jwtCookie = new Cookie("JWT_TOKEN", null);
+        jwtCookie.setHttpOnly(true);
+        jwtCookie.setPath("/");
+        jwtCookie.setMaxAge(0);
+        response.addCookie(jwtCookie);
     }
 
     public User getOne(int user_id){
